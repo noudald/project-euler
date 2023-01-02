abaa3812ea1632c2a89b68110ffb84afbf626c5a
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2023-01-02 12:00:39 +0100

    [823] Add better prime factorization + start of finding cycles

--- 823/main.py
+++ 823/main.py
@@ -14,9 +14,16 @@ S(5, 3) = 15 + 2 + 4 = 21. Also S(10, 100) = 257.
 Find S(10^4, 10^16) mod 1234567891.
 
 Observations:
+    * If the chain has a loop, it will continue to loop. It's sufficient to
+      find a loop in S(10^4, 10^16), such that we don't have to calculate all
+      10^16 steps.
+    * We already have to complete prime factorization of all the numbers we
+      add to the end of the list.
 '''
 
 import numpy as np
+
+from tqdm import tqdm
 
 def smallest_prime_factor(n):
     for i in range(2, int(n**.5) + 1):
@@ -38,3 +45,48 @@ def S_naive(n, m):
     return a.sum().astype(int)
 
 print(S_naive(5, 3), 21)
+
+
+def step(nlist):
+    # Step 1, find smallest primes.
+    smallest_prime = []
+    for elm in nlist:
+        if isinstance(elm, list):
+            smallest_prime.append(elm[0])
+        else:
+            smallest_prime.append(smallest_prime_factor(elm))
+
+    # Step 2, divide by smallest prime number.
+    newlist = []
+    for prime, elm in zip(smallest_prime, nlist):
+        if isinstance(elm, list):
+            if len(elm) == 1:
+                continue
+            else:
+                newlist.append(elm[1:])
+        else:
+            if prime == elm:
+                continue
+            else:
+                newlist.append(elm // prime)
+
+    # Step 3, add product of primes to list.
+    newlist.append(sorted(smallest_prime))
+
+    return newlist
+
+def S(n, m):
+    a = list(range(2, n+1))
+    hist = [a]
+    for i in range(m):
+        a = step(a)
+        if a in hist:
+            print('Found cycle!', i)
+            break
+        hist.append(a)
+    return sum([np.prod(elm) for elm in a])
+
+print(S(5, 3), 21)
+
+for i in range(10, 1000):
+    S(i, 10**16)
